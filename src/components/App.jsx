1548475d9ea8d12a6ca03d573b39b9f71c359004
Miles Mosweu <tmtmosweu@gmail.com>
2024-02-20 23:16:21 +0200

    Implement theme toggle functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,35 @@
+import { useEffect, useState, createContext } from 'react';
+import useLocalStorage from 'use-local-storage';
+
 import Navbar from './Navbar';
 
 import '../assets/scss/components/App.scss';
 
+export const ThemeContext = createContext(null);
+
 const App = () => {
+  const preference = window.matchMedia('(prefers-color-scheme: light)').matches;
+  const [theme, setTheme] = useLocalStorage(
+    'theme',
+    preference ? 'light' : 'dark'
+  );
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+  };
+
+  useEffect(() => {
+    const body = document.querySelector('body');
+
+    body.dataset.theme = theme;
+  }, [theme]);
+
   return (
-    <>
-      <Navbar />
-    </>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="app">
+        <Navbar />
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
--- src/components/Navbar/ThemeToggle.jsx
+++ src/components/Navbar/ThemeToggle.jsx
@@ -1,20 +1,27 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../App';
+
 import { MdOutlineLightMode as LightModeIcon } from 'react-icons/md';
 import { MdOutlineDarkMode as DarkModeIcon } from 'react-icons/md';
 
 const ThemeToggle = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
   return (
-    <button className="nav__theme-toggle" title="Darkmode">
-      <span className="light" title="Lightmode">
-        Dark
-      </span>
+    <button
+      className="nav__theme-toggle"
+      title={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
+      onClick={toggleTheme}
+    >
+      <span className="light">Dark</span>
 
-      <span className="dark" title="Darkmode">
-        Light
-      </span>
+      <span className="dark">Light</span>
 
-      <span className="nav__theme-toggle--slider flex flex-ai-c gap-1">
-        <DarkModeIcon />
-        <span>Dark</span>
+      <span
+        className={`nav__theme-toggle--slider flex flex-ai-c gap-1 ${theme}mode`}
+      >
+        {theme === 'light' ? <LightModeIcon /> : <DarkModeIcon />}
+        <span>{theme === 'light' ? 'Light' : 'Dark'}</span>
       </span>
     </button>
   );
