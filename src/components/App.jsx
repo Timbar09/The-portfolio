e40ca67f0797d2e46b6c6b3c82f86c0f333f83a5
Miles Mosweu <tmtmosweu@gmail.com>
2024-02-20 18:40:01 +0200

    Add utils, base and layout sass partials

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import '../assets/scss/components/App.scss';
+
+const App = () => {
+  return (
+    <>
+      <h1 className="title">Miles Mosweu | Software Developer</h1>
+    </>
+  );
+};
+
+export default App;
