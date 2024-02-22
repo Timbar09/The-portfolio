351afc7414d071693634693b8d779acc72ef658d
Miles Mosweu <tmtmosweu@gmail.com>
2024-02-22 18:48:08 +0200

    Add menu toggle button to Navbar component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
+
 import Logo from '../Logo';
 import ThemeToggle from './ThemeToggle';
+import MenuToggleButton from './MenuToggleButton';
 
 import '../../assets/scss/components/Navbar.scss';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="nav">
-      <div className="container px-2">
+    <nav className={`nav ${isMenuOpen ? 'open' : 'closed'}`}>
+      <div className="container px-2 flex flex-jc-sb flex-ai-c">
+        <MenuToggleButton handleClick={handleToggleMenu} />
         <Logo />
         <ThemeToggle />
       </div>
