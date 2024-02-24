e4a1722048b631bf0f4c0aadd8d77be6a1c7957e
Miles Mosweu <tmtmosweu@gmail.com>
2024-02-25 00:58:02 +0200

    Update App component and add varibles scss partial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,34 @@
-import { useEffect, useState, createContext } from 'react';
-import useLocalStorage from 'use-local-storage';
+import { useEffect, useState, createContext } from "react";
+import useLocalStorage from "use-local-storage";
 
-import Navbar from './Navbar';
-import Overlay from './Overlay';
+import Navbar from "./Navbar";
+import Overlay from "./Overlay";
 
-import '../assets/scss/components/App.scss';
+import "../assets/scss/components/App.scss";
 
 export const ThemeContext = createContext(null);
 
 const App = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const preference = window.matchMedia('(prefers-color-scheme: light)').matches;
+  const preference = window.matchMedia("(prefers-color-scheme: light)").matches;
   const [theme, setTheme] = useLocalStorage(
-    'theme',
-    preference ? 'light' : 'dark'
+    "theme",
+    preference ? "light" : "dark"
   );
 
   const handleToggleMenu = () => {
-    const body = document.querySelector('body');
+    const body = document.querySelector("body");
 
     setIsMenuOpen(!isMenuOpen);
-    body.style.overflow = isMenuOpen ? 'auto' : 'hidden';
+    body.style.overflow = isMenuOpen ? "auto" : "hidden";
   };
 
   const toggleTheme = () => {
-    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+    setTheme(prev => (prev === "light" ? "dark" : "light"));
   };
 
   useEffect(() => {
-    const body = document.querySelector('body');
+    const body = document.querySelector("body");
 
     body.dataset.theme = theme;
   }, [theme]);
@@ -40,61 +40,33 @@ const App = () => {
 
         <Navbar isMenuOpen={isMenuOpen} handleToggleMenu={handleToggleMenu} />
 
-        <div className="lorem-text px-1 py-3">
-          <h1 id="home">Home</h1>
+        <div
+          className="container px-1 py-3"
+          style={{
+            marginTop: "5rem",
+          }}
+        >
+          <h1 className="title">My Awesome Portforlio Website</h1>
 
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Exercitationem, accusantium facere nihil, laboriosam illo iste
-            labore deserunt consectetur quibusdam rerum illum ab laborum id
-            eligendi dolor neque quam alias voluptatibus voluptate? Accusamus
-            laudantium quam placeat eveniet aliquid culpa, cumque perferendis.
-            Ad soluta officia dolorum explicabo, excepturi exercitationem
-            voluptatibus laborum illo praesentium. At quaerat voluptate, et odio
-            explicabo deleniti tempora alias dolore consequatur velit libero
-            recusandae, temporibus hic. Illo soluta commodi quae doloremque,
-            excepturi quo ea molestiae magnam ipsa cum hic aliquid quidem minima
-            nulla vero dignissimos mollitia sit earum velit tenetur nihil
-            consequatur aspernatur at explicabo. Libero modi eos saepe! Dolorum
-            architecto quos ea, eaque, modi a atque repudiandae magni ex numquam
-            illo inventore distinctio esse dolor beatae. Sunt, dolorum quas.
-            Molestias veniam vero quis reiciendis iste error tempore corrupti
-            maiores. Nisi fugit ad quos. Harum nobis eum, inventore totam
-            consectetur quis consequatur at quibusdam officiis aut pariatur ab
-            accusamus nulla delectus, necessitatibus ullam iure earum vero
-            beatae fuga dicta repudiandae. Temporibus excepturi assumenda
-            eligendi unde voluptates ad deleniti pariatur. Incidunt ea delectus
-            dolores eligendi officiis ipsum quod, ad nesciunt, vitae error ullam
-            molestiae magni nemo corporis inventore? Incidunt, quo dolor quasi
-            maiores soluta earum accusamus culpa quis. Error explicabo esse
-            culpa vel laudantium consectetur deserunt aliquam cum beatae
-            consequatur? Aperiam doloremque labore saepe quas eveniet! Ipsum,
-            unde eum autem, tempora illo nobis consectetur sit numquam
-            repudiandae vero incidunt quo eligendi esse hic delectus dolores
-            accusamus provident. Tenetur corrupti expedita explicabo itaque
-            fugiat vitae eveniet, illum odio quidem nihil quas blanditiis
-            consequatur nesciunt maiores! At, repellendus voluptatum illum
-            quisquam reiciendis culpa, labore eligendi possimus quis magni quam
-            repellat. Illum recusandae cupiditate assumenda ut ad unde deserunt
-            laboriosam sint, at, officiis fugiat aperiam cum corrupti accusamus?
-            Accusamus ad tempore odio neque. Voluptate nisi aspernatur
-            necessitatibus distinctio ipsam amet, aliquam praesentium? Aut.
+            This is a simple portfolio website that I built using React. It has
+            a dark mode feature and a responsive design. The website is built
+            using React, Sass, and a few other libraries. The website is hosted
+            on Netlify and the source code is available on GitHub. Feel free to
+            use this website as a template for your own portfolio website. If
+            you have any questions or suggestions, feel free to contact me. I
+            hope you like it! 😊
           </p>
 
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            quos, quae, quia, doloribus quidem quod voluptas nemo voluptatem
-            doloremque tempora tenetur. Quisquam, quos. Quisquam, quos.
-            Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos.
-            Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos.
-            Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos.
-            Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos.
-            Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos.
-            Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos.
-            Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos.
-            Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos.
-            Quisquam, quos. Quisquam, quos. Quisquam, quos. Quisquam, quos.
-            Quisquam,
+            I am a web developer with a passion for building beautiful and
+            functional websites. I have experience in building websites using
+            HTML, CSS, JavaScript, React, and other web technologies. I am also
+            familiar with web design principles and best practices. I am always
+            looking for new opportunities to learn and grow as a developer. I am
+            open to freelance work and other opportunities. If you have a
+            project that you would like to discuss, feel free to contact me. I
+            would love to hear from you! 😊
           </p>
         </div>
       </div>
