03c7de60a850f0fc18029a6872435c77f756680c
Miles Mosweu <tmtmosweu@gmail.com>
2024-02-29 18:49:00 +0200

    Add section title

--- src/sections/ProjectsSection/index.jsx
+++ src/sections/ProjectsSection/index.jsx
@@ -3,7 +3,11 @@ import "../../assets/scss/sections/ProjectsSection.scss";
 const ProjectsSection = () => {
   return (
     <section name="projects" id="projects" className="project__section section">
-      Projects Section
+      <div className="container">
+        <h2 class="section__title">
+          <span class="underlined">My</span> <span>Projects</span>
+        </h2>
+      </div>
     </section>
   );
 };
