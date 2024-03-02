dbfcd1e5ffc4cb4b91e88e0e5ad559a096d6c40e
Miles Mosweu <tmtmosweu@gmail.com>
2024-03-02 15:07:40 +0200

    Add close button to project modal

--- src/sections/ProjectsSection/ProjectModal.jsx
+++ src/sections/ProjectsSection/ProjectModal.jsx
@@ -1,14 +1,21 @@
 import { useContext } from "react";
 import { ProjectModalContext } from "../../components/App";
 
+import { IoCloseCircle as CloseModalIcon } from "react-icons/io5";
+
 const ProjectModal = () => {
   const { toggleProjectModal, selectedProject } =
     useContext(ProjectModalContext);
 
   return (
     <div className="project__modal p-2">
-      <button type="button" onClick={toggleProjectModal}>
-        Close
+      <button
+        className="project__modal--close-button grid"
+        type="button"
+        onClick={toggleProjectModal}
+        title="Close project modal"
+      >
+        <CloseModalIcon />
       </button>
 
       <h2>{selectedProject.title}</h2>
