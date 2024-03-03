57a0fa94d93de07060b9f6d818a502e968eed353
Miles Mosweu <tmtmosweu@gmail.com>
2024-03-03 12:28:18 +0200

    Add focus trapping to ProjectModal

--- src/sections/ProjectsSection/ProjectModal.jsx
+++ src/sections/ProjectsSection/ProjectModal.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { ProjectModalContext } from "../../components/App";
 
 import Button from "../../components/Button";
@@ -8,11 +8,57 @@ import { IoCloseCircle as CloseModalIcon } from "react-icons/io5";
 import { FaGitAlt as SourceCodeIcon } from "react-icons/fa6";
 
 const ProjectModal = () => {
+  const modalRef = useRef();
   const { toggleProjectModal, selectedProject, isProjectModalOpen } =
     useContext(ProjectModalContext);
 
+  // When the modal is open, add a keydown event listener to trap the focus
+  useEffect(() => {
+    if (isProjectModalOpen) {
+      // Get all focusable elements within the modal
+      const focusableElements = modalRef.current.querySelectorAll(
+        'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+      );
+      const firstFocusableElement = focusableElements[0];
+      const lastFocusableElement =
+        focusableElements[focusableElements.length - 1];
+
+      const handleKeyDown = (event) => {
+        if (event.key === "Tab") {
+          // If the shift key is held down and the first focusable element is focused, move the focus to the last focusable element
+          if (
+            event.shiftKey &&
+            document.activeElement === firstFocusableElement
+          ) {
+            lastFocusableElement.focus();
+            event.preventDefault();
+          }
+          // If the shift key is not held down and the last focusable element is focused, move the focus to the first focusable element
+          else if (
+            !event.shiftKey &&
+            document.activeElement === lastFocusableElement
+          ) {
+            firstFocusableElement.focus();
+            event.preventDefault();
+          }
+        }
+      };
+
+      // Immediately move the focus to the first focusable element
+      firstFocusableElement.focus();
+
+      window.addEventListener("keydown", handleKeyDown);
+
+      return () => {
+        // When the modal is closed, remove the keydown event listener
+        window.removeEventListener("keydown", handleKeyDown);
+      };
+    }
+  }, [isProjectModalOpen]);
+
   return (
     <div
+      ref={modalRef}
       className={`project__modal p-2 ${isProjectModalOpen ? "open" : "closed"}`}
     >
       <div className="project__modal--container flex flex-col">
