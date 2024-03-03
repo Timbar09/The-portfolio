d95cdea579434f742fe557cce9294a8795806481
Miles Mosweu <tmtmosweu@gmail.com>
2024-03-03 10:51:21 +0200

    Refactor ProjectsSection.scss and ProjectModal.jsx

--- src/sections/ProjectsSection/ProjectModal.jsx
+++ src/sections/ProjectsSection/ProjectModal.jsx
@@ -14,24 +14,20 @@ const ProjectModal = () => {
     <div
       className={`project__modal p-2 ${isProjectModalOpen ? "open" : "closed"}`}
     >
-      <div className="project__modal--container">
-        <button
-          className="project__modal--close-button grid"
-          type="button"
-          onClick={toggleProjectModal}
-          title="Close project modal"
-        >
-          <CloseModalIcon />
-        </button>
+      <div className="project__modal--container flex flex-col">
+        <header className="project__modal--header flex flex-jc-sb flex-wrap gap-2 py-2">
+          <button
+            className="project__modal--header__close-button grid"
+            type="button"
+            onClick={toggleProjectModal}
+            title="Close project modal"
+          >
+            <CloseModalIcon />
+          </button>
 
-        <div className="project__modal--image">
-          <img src={selectedProject.bgImage} alt={selectedProject.title} />
-        </div>
-
-        <div className="project__modal--header flex flex-jc-sb flex-wrap gap-2 p-2">
           <h2>{selectedProject.title}</h2>
 
-          <div className="project__modal--buttons flex flex-wrap gap-2">
+          <div className="project__modal--header__buttons flex flex-wrap gap-2">
             <Button
               type="primary"
               name="Live Demo"
@@ -45,17 +41,33 @@ const ProjectModal = () => {
               icon={<SourceCodeIcon />}
             />
           </div>
+        </header>
+
+        <div className="project__modal--body">
+          <div className="project__modal--body__image">
+            <img src={selectedProject.bgImage} alt={selectedProject.title} />
+          </div>
+
+          <div className="project__modal--body__text p-2 flex_md flex-ai-fs gap-2">
+            <div className="project__modal--body__text--description">
+              <p>{selectedProject.description}</p>
+              <p>{selectedProject.description}</p>
+              <p>{selectedProject.description}</p>
+              <p>{selectedProject.description}</p>
+            </div>
+
+            <ul className="project__modal--body__text--tech flex flex-ai-c flex-wrap gap-1">
+              {selectedProject.tech.map((item, index) => (
+                <li
+                  key={index}
+                  className="project__card--body__text--tech__item p-1"
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-
-        <p className="px-2">{selectedProject.description}</p>
-
-        <ul className="project__modal--tech project__card--tech flex flex-ai-c gap-1 px-2">
-          {selectedProject.tech.map((item, index) => (
-            <li key={index} className="project__card--tech__item">
-              {item}
-            </li>
-          ))}
-        </ul>
       </div>
     </div>
   );
