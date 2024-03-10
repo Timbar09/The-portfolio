21bc5ab2234cf180d41da5f13e40e5129eefe417
Miles Mosweu <tmtmosweu@gmail.com>
2024-03-10 16:44:03 +0200

    Update typography and button styles, and refactor soft skills section

--- src/sections/AboutSection/AboutSoftSkills.jsx
+++ src/sections/AboutSection/AboutSoftSkills.jsx
@@ -5,64 +5,52 @@ import { MdOutlineHandshake as CollabIcon } from "react-icons/md";
 import { LuTimer as TimeIcon } from "react-icons/lu";
 import { LuPocketKnife as AdaptIcon } from "react-icons/lu";
 
+const softSkills = [
+  {
+    title: "Communication",
+    description:
+      "I've learned to communicate clearly and effectively with people from different backgrounds and nationalities.",
+    icon: <CommsIcon />,
+  },
+  {
+    title: "Collaboration",
+    description:
+      "I've worked with developers from all over the world, and I've learned to work in a team, manage projects, and meet deadlines.",
+    icon: <CollabIcon />,
+  },
+  {
+    title: "Time Management",
+    description:
+      "I've learned to manage my time effectively and efficiently, especially when working on multiple projects.",
+    icon: <TimeIcon />,
+  },
+  {
+    title: "Adaptability",
+    description:
+      "I've learned to adapt to new technologies, tools, and environments.",
+    icon: <AdaptIcon />,
+  },
+];
+
 const AboutSoftSkills = () => {
   return (
     <div className="about__soft-skills">
-      <h3 className="about__subtitle">Soft Skills</h3>
-
-      <p className="about__soft-skills--description">
-        Certain skills are not only essential for the workplace but also for
-        life in general. Here are some of the soft skills I've developed over
-        the years, particularly during my at{" "}
+      <h3 className="about__subtitle">
+        I have honed essential soft skills applicable to both life and the
+        workplace, particularly during my time at{" "}
         <MyLink linkText="Microverse" linkTo="https://www.microverse.org/" />:
-      </p>
+      </h3>
 
       <ul className="about__soft-skills--list grid grid-col grid-gap-2">
-        <li className="about__soft-skills--list__item p-2">
-          <h4 className="flex flex-ai-c gap-1">
-            <CommsIcon /> Communication:
-          </h4>
+        {softSkills.map((skill, index) => (
+          <li key={index} className="about__soft-skills--list__item p-2">
+            <h4 className="flex flex-ai-c gap-1">
+              {skill.icon} {skill.title}:
+            </h4>
 
-          <p>
-            I've learned to communicate clearly and effectively with people from
-            different backgrounds and nationalities.
-          </p>
-        </li>
-
-        <li className="about__soft-skills--list__item p-2">
-          <h4 className="flex flex-ai-c gap-1">
-            <CollabIcon />
-            Collaboration:
-          </h4>
-
-          <p>
-            I've worked with developers from all over the world, and I've
-            learned to work in a team, manage projects, and meet deadlines.
-          </p>
-        </li>
-
-        <li className="about__soft-skills--list__item p-2">
-          <h4 className="flex flex-ai-c gap-1">
-            <TimeIcon />
-            Time Management:
-          </h4>
-
-          <p>
-            I've learned to manage my time effectively and efficiently,
-            especially when working on multiple projects.
-          </p>
-        </li>
-
-        <li className="about__soft-skills--list__item p-2">
-          <h4 className="flex flex-ai-c gap-1">
-            <AdaptIcon />
-            Adaptability:
-          </h4>
-
-          <p>
-            I've learned to adapt to new technologies, tools, and environments.
-          </p>
-        </li>
+            <p>{skill.description}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
