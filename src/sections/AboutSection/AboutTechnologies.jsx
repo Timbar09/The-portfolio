e6c77baa5e49daf5219b85da0163f7e81d4abd1c
Miles Mosweu <tmtmosweu@gmail.com>
2024-03-14 19:52:23 +0200

    Refactor AboutTechnologies component and Tabs.scss

--- src/sections/AboutSection/AboutTechnologies.jsx
+++ src/sections/AboutSection/AboutTechnologies.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { useContext } from "react";
+import { ThemeContext } from "../../components/App";
 
 import { TabButtons, TabContent } from "../../components/Tabs";
 
@@ -21,104 +23,6 @@ import {
   FaGithub as GithubIcon,
   FaGitlab as GitlabIcon,
 } from "react-icons/fa6";
-
-const techData = [
-  {
-    id: 1,
-    name: "JavaScript",
-    icon: <JsIcon style={{ color: "#f0db4f" }} />,
-    category: "front-end",
-    description: "High-level language",
-  },
-
-  {
-    id: 2,
-    name: "React",
-    icon: <ReactIcon style={{ color: "#61dafb" }} />,
-    category: "front-end",
-    description: "UI JavaScript library",
-  },
-
-  {
-    id: 3,
-    name: "Tailwind CSS",
-    icon: <TailwindIcon style={{ color: "#06b6d4" }} />,
-    category: "front-end",
-    description: "Utility-first CSS framework",
-  },
-
-  {
-    id: 4,
-    name: "Sass",
-    icon: <SassLogo style={{ color: "#cc6699" }} />,
-    category: "front-end",
-    description: "CSS preprocessor",
-  },
-
-  {
-    id: 5,
-    name: "Bootstrap",
-    icon: <BootstrapIcon style={{ color: "#7952b3" }} />,
-    category: "front-end",
-    description: "CSS framework",
-  },
-
-  {
-    id: 6,
-    name: "Ruby",
-    icon: <RubyIcon style={{ color: "#cc342d" }} />,
-    category: "back-end",
-    description: "Dynamic language",
-  },
-
-  {
-    id: 7,
-    name: "Rails",
-    icon: <RailsIcon style={{ color: "#cc0000" }} />,
-    category: "back-end",
-    description: "Ruby web app framework",
-  },
-
-  {
-    id: 8,
-    name: "PostgreSQL",
-    icon: <PostgresqlIcon style={{ color: "#336791" }} />,
-    category: "back-end",
-    description: "Open source relational database",
-  },
-
-  {
-    id: 9,
-    name: "Git",
-    icon: <GitIcon style={{ color: "#f34f29" }} />,
-    category: "tools",
-    description: "Distributed version control",
-  },
-
-  {
-    id: 10,
-    name: "GitHub",
-    icon: <GithubIcon style={{ color: "#181717" }} />,
-    category: "tools",
-    description: "Web-based Git repository hosting",
-  },
-
-  {
-    id: 11,
-    name: "GitLab",
-    icon: <GitlabIcon style={{ color: "#fca326" }} />,
-    category: "tools",
-    description: "Web-based DevOps lifecycle tool",
-  },
-
-  {
-    id: 12,
-    name: "VSCode",
-    icon: <VscodeIcon style={{ color: "#007acc" }} />,
-    category: "tools",
-    description: "Code editor",
-  },
-];
 
 const techItemAnimationProps = {
   initial: { opacity: 0, y: 20 },
@@ -157,6 +61,109 @@ const RenderTechItems = ({ data, activeTab }) => {
 
 const AboutTechnologies = () => {
   const [activeTab, setActiveTab] = useState("all");
+  const { theme } = useContext(ThemeContext);
+
+  const techData = [
+    {
+      id: 1,
+      name: "JavaScript",
+      icon: <JsIcon style={{ color: "#f0db4f" }} />,
+      category: "front-end",
+      description: "High-level language",
+    },
+
+    {
+      id: 2,
+      name: "React",
+      icon: <ReactIcon style={{ color: "#61dafb" }} />,
+      category: "front-end",
+      description: "UI JavaScript library",
+    },
+
+    {
+      id: 3,
+      name: "Tailwind CSS",
+      icon: <TailwindIcon style={{ color: "#06b6d4" }} />,
+      category: "front-end",
+      description: "Utility-first CSS framework",
+    },
+
+    {
+      id: 4,
+      name: "Sass",
+      icon: <SassLogo style={{ color: "#cc6699" }} />,
+      category: "front-end",
+      description: "CSS preprocessor",
+    },
+
+    {
+      id: 5,
+      name: "Bootstrap",
+      icon: <BootstrapIcon style={{ color: "#7952b3" }} />,
+      category: "front-end",
+      description: "CSS framework",
+    },
+
+    {
+      id: 6,
+      name: "Ruby",
+      icon: <RubyIcon style={{ color: "#cc342d" }} />,
+      category: "back-end",
+      description: "Dynamic language",
+    },
+
+    {
+      id: 7,
+      name: "Rails",
+      icon: <RailsIcon style={{ color: "#cc0000" }} />,
+      category: "back-end",
+      description: "Ruby web app framework",
+    },
+
+    {
+      id: 8,
+      name: "PostgreSQL",
+      icon: <PostgresqlIcon style={{ color: "#336791" }} />,
+      category: "back-end",
+      description: "Open source relational database",
+    },
+
+    {
+      id: 9,
+      name: "Git",
+      icon: <GitIcon style={{ color: "#f34f29" }} />,
+      category: "tools",
+      description: "Distributed version control",
+    },
+
+    {
+      id: 10,
+      name: "GitHub",
+      icon: (
+        <GithubIcon
+          style={{ color: `${theme === "light" ? "#24292e" : "#f0f6fc"}` }}
+        />
+      ),
+      category: "tools",
+      description: "Web-based Git repository hosting",
+    },
+
+    {
+      id: 11,
+      name: "GitLab",
+      icon: <GitlabIcon style={{ color: "#fca326" }} />,
+      category: "tools",
+      description: "Web-based DevOps lifecycle tool",
+    },
+
+    {
+      id: 12,
+      name: "VSCode",
+      icon: <VscodeIcon style={{ color: "#007acc" }} />,
+      category: "tools",
+      description: "Code editor",
+    },
+  ];
 
   return (
     <div className="about__tech">
