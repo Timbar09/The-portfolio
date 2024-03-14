303bacb607b3cbd30b8ca6572271521811e17d70
Miles Mosweu <tmtmosweu@gmail.com>
2024-03-14 23:13:59 +0200

    Refactor component structure and add layout components

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container container px-2 py-3">
+        <p>
+          Made with ❤️ by{" "}
+          <a href="" target="_blank" rel="noreferrer">
+            Miles Mosweu
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
