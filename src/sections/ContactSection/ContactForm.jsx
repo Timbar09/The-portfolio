232dbefc8c04fa0674d0f448da0ad911684080b4
Miles Mosweu <tmtmosweu@gmail.com>
2024-03-16 08:29:13 +0200

    Add form field styling and update contact form layout

--- src/sections/ContactSection/ContactForm.jsx
+++ src/sections/ContactSection/ContactForm.jsx
@@ -1,9 +1,12 @@
 import FormField from "./FormField";
+import Button from "../../components/Button";
 
 const ContactForm = () => {
   return (
-    <form className="contact__form">
-      <FormField />
+    <form className="contact__form p-2 grid grid-col grid-gap-2">
+      <FormField label="Name *" id="name" required={true} />
+
+      <Button type="primary" name="Send" form={true} />
     </form>
   );
 };
--- src/sections/ContactSection/FormField.jsx
+++ src/sections/ContactSection/FormField.jsx
@@ -2,20 +2,34 @@ const FormField = ({
   label = "Label text",
   type = "text",
   id = "input-id",
-  placeholder = "Placeholder text",
+  required = false,
 }) => {
   return (
     <div className="form-field">
       <label htmlFor={id} className="form-field__label">
-        {label}
+        {label.split("").map((letter, index) => (
+          <span
+            key={index}
+            style={{ transitionDelay: `${index * 50}ms` }}
+            className="form-field__label--letter"
+          >
+            {letter}
+          </span>
+        ))}
       </label>
+
       <input
         type={type}
         id={id}
         name={id}
-        placeholder={placeholder}
-        className="form-field__input"
+        placeholder={label}
+        required={required}
+        minLength="3"
+        maxLength="50"
+        className="form-field__input p-1"
       />
+
+      <div className="form-field__indicator" />
     </div>
   );
 };
