7cb639044b5bd0300cae80b857e89040f5cd84ce
Miles Mosweu <tmtmosweu@gmail.com>
2024-03-21 10:05:13 +0200

    Hide Navbar on scrolldown and reveal on scrollup

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { MenuContext } from "../App";
 import { useMediaQuery } from "../../hooks/useMediaQuery";
 
@@ -10,6 +10,26 @@ import "../../assets/scss/components/Navbar.scss";
 const Navbar = () => {
   const { isMenuOpen } = useContext(MenuContext);
   const isMobile = useMediaQuery("sm", "down");
+  let lastScrollY = 0;
+
+  const NavAppeareance = () => {
+    const nav = document.querySelector(".nav");
+    if (isMobile) return;
+
+    window.scrollY > lastScrollY
+      ? nav.classList.add("hidden")
+      : nav.classList.remove("hidden");
+
+    lastScrollY = window.scrollY;
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", NavAppeareance);
+
+    return () => {
+      window.removeEventListener("scroll", NavAppeareance);
+    };
+  }, [lastScrollY, isMobile]);
 
   return (
     <nav className={`nav ${isMobile && (isMenuOpen ? "open" : "closed")}`}>
