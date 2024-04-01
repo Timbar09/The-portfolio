a65971aeeba51afdc54b35a2a8af798683a26eac
Miles Mosweu <tmtmosweu@gmail.com>
2024-04-01 11:51:27 +0200

    Import project image modules and refactor data extratration

--- src/sections/ProjectsSection/ProjectCard.jsx
+++ src/sections/ProjectsSection/ProjectCard.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ProjectModalContext } from "../../components/App";
 
 import TechItem from "./TechItem";
@@ -7,24 +7,35 @@ import Button from "../../components/Button";
 const ProjectCard = ({ data }) => {
   const { toggleProjectModal, setSelectedProject } =
     useContext(ProjectModalContext);
+  const [bgImage, setBgImage] = useState("");
+  const briefDescription = data.description.brief;
+
+  useEffect(() => {
+    const loadImg = async () => {
+      const imgModule = await import(
+        /* @vite-ignore */
+        `/src/assets/images/projects/${data.image}`
+      );
+      setBgImage(imgModule.default);
+    };
+    loadImg();
+  }, [data.image]);
+
+  const cardStyle = {
+    backgroundImage: `url(${bgImage})`,
+  };
 
   return (
-    <li
-      className={`project__card project__card--${data.title
-        .toLowerCase()
-        .replace(/\s/g, "-")}`}
-      aria-label={data.title}
-      style={{ backgroundImage: `url(${data.bgImage})` }}
-    >
+    <li className="project__card" aria-label={data.title} style={cardStyle}>
       <div className="project__card--container grid grid-pi-c p-2">
         <div className="project__card--info flex flex-col gap-2">
           <h3>{data.title}</h3>
 
-          <p>{data.description}</p>
+          <p>{briefDescription}</p>
 
           <div className="project__card--details flex flex-jc-sb flex-wrap gap-1">
             <ul className="project__card--tech__list flex flex-ai-c flex-jc-sb gap-1">
-              {data.tech.map((item, index) => (
+              {data.tech.slice(0, 3).map((item, index) => (
                 <TechItem key={index} item={item} />
               ))}
             </ul>
