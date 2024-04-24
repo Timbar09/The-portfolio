e65b727af6dee9554984d290cd2c1c47b0b77556
Miles Mosweu <tmtmosweu@gmail.com>
2024-04-24 17:59:37 +0200

    Add projectImages.js to manage project image imports

---
+++ src/sections/ProjectsSection/projectImages.js
@@ -0,0 +1,13 @@
+import consoleHubBgImage from "../../assets/images/projects/consolehub.png";
+import budgettrailBgImage from "../../assets/images/projects/budgettrail.png";
+import spacetravelershubBgImage from "../../assets/images/projects/spacetravelershub.png";
+import blogisterBgImage from "../../assets/images/projects/blogister.png";
+import conferenceBgImage from "../../assets/images/projects/conference.png";
+
+export default {
+  consolehub: consoleHubBgImage,
+  budgettrail: budgettrailBgImage,
+  spacetravelershub: spacetravelershubBgImage,
+  blogister: blogisterBgImage,
+  conference: conferenceBgImage,
+};
