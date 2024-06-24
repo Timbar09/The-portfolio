b3efb2edc0e5b5de4d3b116c3c321249f142d3a7
Miles Mosweu <tmtmosweu@gmail.com>
2024-06-24 20:24:50 +0200

    refactor: Update project card styles, add project logo, and improve transitions

--- src/sections/ProjectsSection/projectImages.js
+++ src/sections/ProjectsSection/projectImages.js
@@ -1,9 +1,14 @@
 import placeHolderLogo from "../../assets/images/projects/project-logo-placeholder.jpg";
 import consoleHubBgImage from "../../assets/images/projects/consolehub.png";
+import consoleHubLogoImage from "../../assets/images/projects/consolehub-logo.png";
 import budgettrailBgImage from "../../assets/images/projects/budgettrail.png";
+import budgettrailLogoImage from "../../assets/images/projects/budgettrail-logo.png";
 import spacetravelershubBgImage from "../../assets/images/projects/spacetravelershub.png";
+import spacetravelershubLogoImage from "../../assets/images/projects/spacetravelershub-logo.png";
 import blogisterBgImage from "../../assets/images/projects/blogister.png";
+import blogisterLogoImage from "../../assets/images/projects/blogister-logo.png";
 import conferenceBgImage from "../../assets/images/projects/conference.png";
+import conferenceLogoImage from "../../assets/images/projects/conference-logo.png";
 
 export default {
   dummy: {
@@ -12,22 +17,22 @@ export default {
   },
   consolehub: {
     bgImage: consoleHubBgImage,
-    logo: placeHolderLogo,
+    logo: consoleHubLogoImage,
   },
   budgettrail: {
     bgImage: budgettrailBgImage,
-    logo: placeHolderLogo,
+    logo: budgettrailLogoImage,
   },
   spacetravelershub: {
     bgImage: spacetravelershubBgImage,
-    logo: placeHolderLogo,
+    logo: spacetravelershubLogoImage,
   },
   blogister: {
     bgImage: blogisterBgImage,
-    logo: placeHolderLogo,
+    logo: blogisterLogoImage,
   },
   conference: {
     bgImage: conferenceBgImage,
-    logo: placeHolderLogo,
+    logo: conferenceLogoImage,
   },
 };
