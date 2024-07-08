5dce5d05b7dc515e419b17f03b25451aa647e0da
Miles Mosweu <tmtmosweu@gmail.com>
2024-07-08 18:03:08 +0200

    refactor: Update social media links with ref parameter

--- src/components/SocialLinkTree.jsx
+++ src/components/SocialLinkTree.jsx
@@ -4,20 +4,22 @@ import { FaXTwitter as XIcon } from "react-icons/fa6";
 
 import "../assets/scss/components/SocialLinkTree.scss";
 
+const ref = "?ref=milesmosweu.netlify.app";
+
 const socialLinks = [
   {
     name: "GitHub",
-    url: "https://github.com/Timbar09",
+    url: `https://github.com/Timbar09${ref}`,
     icon: <GithubIcon />,
   },
   {
     name: "LinkedIn",
-    url: "https://www.linkedin.com/in/miles-mosweu09",
+    url: `https://www.linkedin.com/in/miles-mosweu09${ref}`,
     icon: <LinkedInIcon />,
   },
   {
     name: "X / Twitter",
-    url: "https://twitter.com/Milez09",
+    url: `https://twitter.com/Milez09${ref}`,
     icon: <XIcon />,
   },
 ];
@@ -27,12 +29,7 @@ const SocialLinkTree = () => {
     <ul className="social__list flex gap-2 p-1">
       {socialLinks.map((link, index) => (
         <li key={index} className="social__item">
-          <a
-            href={link.url}
-            target="_blank"
-            rel="noreferrer"
-            className="social__item--link grid"
-          >
+          <a href={link.url} target="_blank" rel="noreferrer" className="social__item--link grid">
             {link.icon}
             <span className="social__item--name">{link.name}</span>
           </a>
