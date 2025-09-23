314358e5451c42a12597910403a897e91b99153f
mile09 <tmtmosweu@gmail.com>
2025-09-23 15:04:34 +0200

    Enhance Tooltip component to manage positioning dynamically and improve overflow handling

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -7,7 +7,9 @@ import "../assets/scss/components/Tooltip.scss";
 
 const Tooltip = ({ children, isLink = false, styles = {} }) => {
   const [visible, setVisible] = useState(false);
+  const [position, setPosition] = useState({});
   const parentRef = useRef(null);
+  const tooltipRef = useRef(null);
 
   useEffect(() => {
     const parent = parentRef.current?.parentNode;
@@ -25,6 +27,40 @@ const Tooltip = ({ children, isLink = false, styles = {} }) => {
     };
   }, []);
 
+  useEffect(() => {
+    if (visible && parentRef.current && tooltipRef.current) {
+      const tooltipRect = tooltipRef.current.getBoundingClientRect();
+
+      const difference = {
+        left: tooltipRect.left,
+        right: window.innerWidth - tooltipRect.right,
+      };
+
+      const sideCloserToEdge =
+        difference.left < difference.right ? "left" : "right";
+
+      const isOverflowing = difference[sideCloserToEdge] < 0;
+
+      if (!isOverflowing) return;
+
+      let left = "50%";
+      let translate = "-50% 0";
+      let right = "auto";
+
+      if (sideCloserToEdge === "left") {
+        left = "0px";
+        right = "auto";
+        translate = "none";
+      } else {
+        right = "0px";
+        left = "auto";
+        translate = "none";
+      }
+
+      setPosition({ left, right, translate });
+    }
+  }, [visible]);
+
   const tooltipAnimationProps = {
     initial: { opacity: 0, y: -10, rotate: 0 },
     animate: {
@@ -34,6 +70,12 @@ const Tooltip = ({ children, isLink = false, styles = {} }) => {
     },
     exit: { opacity: 0, y: -10 },
     transition: { duration: 0.4 },
+  };
+
+  const customVariables = {
+    "--left": position.left === "0px" ? "16px" : position.left,
+    "--right": position.right === "0px" ? "16px" : position.right,
+    "--translate": position.translate,
   };
 
   return (
@@ -47,10 +89,14 @@ const Tooltip = ({ children, isLink = false, styles = {} }) => {
       {visible && (
         <motion.span
           key={crypto.randomUUID()}
-          ref={parentRef}
+          ref={tooltipRef}
           className="tooltip"
           {...tooltipAnimationProps}
-          style={styles}
+          style={{
+            ...styles,
+            ...position,
+            ...customVariables,
+          }}
         >
           <p className="tooltip__text">{children}</p>
           {isLink && (
