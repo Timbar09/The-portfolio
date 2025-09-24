0049b4f026dbde99f0eef76ac731fb58c31b8a3b
mile09 <tmtmosweu@gmail.com>
2025-09-24 15:41:40 +0200

    Enhance Logo component with tooltip for homepage navigation and improve SCSS styling for logo elements

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,6 +5,7 @@ import { MenuContext } from "./App";
 import { useMediaQuery } from "../hooks/useMediaQuery";
 
 import "../assets/scss/components/Logo.scss";
+import Tooltip from "./Tooltip";
 
 const Logo = ({ className = "logo__header" }) => {
   const { toggleMenu } = useContext(MenuContext);
@@ -14,9 +15,35 @@ const Logo = ({ className = "logo__header" }) => {
     <NavLink
       to="/"
       className={`logo ${className}`}
+      aria-label="Go to homepage"
       onClick={isMobile ? toggleMenu : undefined}
     >
-      <span>{"{"}</span> MILES <span>{"}"}</span>
+      {({ isActive }) => (
+        <>
+          <span className="logo__curly-brace logo__curly-brace--left">
+            {"{"}
+          </span>{" "}
+          MILES{" "}
+          <span className="logo__curly-brace logo__curly-brace--right">
+            {"}"}
+          </span>
+          {isActive ? (
+            <Tooltip styles={{ fontSize: "0.975rem", fontWeight: "300" }}>
+              You are home
+            </Tooltip>
+          ) : (
+            <Tooltip
+              isLink
+              styles={{
+                fontSize: "0.975rem",
+                fontWeight: "300",
+              }}
+            >
+              Go to homepage
+            </Tooltip>
+          )}
+        </>
+      )}
     </NavLink>
   );
 };
